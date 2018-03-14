bab41b86b07a72238541ce1fe1eee19bc3c8caf6
jconroy5 <jconroy5@luc.edu>
2018-03-14 13:44:28 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+
+GREEN = (0, 170, 100)
+WHITE = (255, 255, 255)
+
+window = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Tristram')
+window.fill(GREEN)
+pygame.draw.rect(window, WHITE, (200, 200, 100, 50))
+pygame.display.flip()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
