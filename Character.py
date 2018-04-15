cb023bc3230852699e516a851ef66671a80a5553
Dangey <32312177+Dangey@users.noreply.github.com>
2018-04-15 14:40:59 -0500

    Add files via upload

--- Character.py
+++ Character.py
@@ -1,10 +1,17 @@
-<<<<<<< current
-class Player(object):
+import pygame
 
-    def __init__(self, hp, speed, damage):
-        self.hp = hp
-        self.speed = speed
-        self.damage = damage
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self, winWidth, winHeight, color):
+        pygame.sprite.Sprite._init_(self)
+        self.image = pygame.Surface
+        self.hp = 10.0
+        self.speed = 1.0
+        self.damage = 1.0
+        self.image = pygame.Surface((30,30))
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.center = (winWidth / 2, winHeight / 2)
 
     def get_hp(self):
         return self.hp
@@ -34,17 +41,3 @@ class Enemy(object):
 
     def get_damage(self):
         return self.damage
-=======
-import pygame, importlib, runGame
-
-class Character:
-    #Base class for player character
-
-    def __init__(self, hp, speed, damage):
-        self.hp = 3.0
-        self.speed = 1.0
-        self.damage = 1.0
-
-    def drawCharacter(self, window, color):
-        pygame.draw.rect(window, color, (200, 200, 100, 50))
->>>>>>> before discard
--- runGame.py
+++ runGame.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import os
 import Character
 
 from Character import Player, Enemy
@@ -8,38 +9,56 @@ BLACK = (  0,   0,   0)
 GREEN = (  0, 170, 100)
 WHITE = (255, 255, 255)
 
-TITLE = "Tristram"
+TITLE = "75mm"
 WIN_HEIGHT = 576
 WIN_WIDTH = 1024
+
+game_dir = os.path.dirname(__file__)
+assets_dir = os.path.join(game_dir, "Assets")
+images_dir = os.path.join(assets_dir, "Images")
+music_dir = os.path.join(assets_dir, "Music")
+intro_song = os.path.join(music_dir, "bensound-epic.mp3")
 # ******************************************************************************** #
 def main():
     pygame.init()
 
     game_window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     pygame.display.set_caption(TITLE)
+    #**************************GAME*LOOP*********************************#
+    while True:
+        show_start_screen(game_window)
 
-    show_start_screen(game_window)
+        game_sprites = pygame.sprite.Group()
 
+    #*******************************************************************#
+    pygame.quit()
+    sys.exit()
 
 def show_start_screen(window):
     title_text = "Press -SPACE- to begin"
 
     window.fill(BLACK)
     pygame.display.update()
-    textRender(window, title_text, 60, WIN_WIDTH / 2, WIN_HEIGHT - 40)
+    pygame.mixer.init()
+    pygame.mixer.music.load(intro_song)
+    pygame.mixer.music.play()
+    textRender(window, TITLE, 300, WIN_WIDTH / 2, 50)
+    textRender(window, title_text, 60, WIN_WIDTH / 2, WIN_HEIGHT - 45)
 
-    events = pygame.event.get()
     while True:
-        for event in events:
+        for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
-                if event.type == pygame.K_SPACE:
+                if event.key == pygame.K_SPACE:
                     return
-                if event.type == pygame.K_ESCAPE:
+                if event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     sys.exit()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
 
-
-def playerMove(player_speed, location)
+#def draw_map(window):
 
 def textRender(surface, text, size, x, y):
     font_match = pygame.font.match_font('terminal')
