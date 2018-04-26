f455c3d30fe69b7d0b0ae5bf61c0e068107f720c
Dangey <32312177+Dangey@users.noreply.github.com>
2018-04-25 20:37:04 -0500

    Updated 4/25 8:37

---
+++ Tristram/Character.py
@@ -0,0 +1,47 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self, winWidth, winHeight, image):
+        pygame.sprite.Sprite.__init__(self)
+        self.hp = 10.0
+        self.speed = 0.3
+        self.damage = 1.0
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.x = winWidth / 2
+        self.y = winHeight / 2
+        self.rect.center = (self.x, self.y)
+
+    #def moveBody(self, direction):
+
+    #def moveHead(self, direction):
+
+    def get_hp(self):
+        return self.hp
+
+    def get_speed(self):
+        return self.speed
+
+    def get_damage(self):
+        return self.damage
+
+    # def draw_set_player(window, x_rect, y_rect ):
+
+    # def move_player(self, speed):
+
+class Enemy(object):
+
+    def __init__(self, hp, speed, damage):
+        self.hp = hp
+        self.speed = speed
+        self.damage = damage
+
+    def get_hp(self):
+        return self.hp
+
+    def get_speed(self):
+        return self.speed
+
+    def get_damage(self):
+        return self.damage
